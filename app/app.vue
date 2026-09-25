9514e136b5f46b9dd620cf3421763fb9fd555571
probablyjassin <123012687+probablyjassin@users.noreply.github.com>
2026-09-25 16:47:05 +0200

    add SEO title and description to template app.vue

--- app/app.vue
+++ app/app.vue
@@ -11,8 +11,8 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'The Fluxer Directory'
+const description = 'A place for users to discover self-hosted Fluxer instances and list their own'
 
 useSeoMeta({
   title,
