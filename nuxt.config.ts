a2502c932aca235a5de8dec761d2129a0de8a3f8
probablyjassin <123012687+probablyjassin@users.noreply.github.com>
2026-09-25 16:39:40 +0200

    initial commit (nuxtjs install)

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/hints',
+    '@nuxtjs/i18n',
+    '@nuxtjs/seo',
+    '@pinia/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2026-06-30',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
